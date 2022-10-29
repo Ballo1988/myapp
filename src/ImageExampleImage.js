29af41de3bb80ce9b04857c4141619a222d43316
Ballo1988 <rounderjards@gmail.com>
2022-10-29 18:36:30 -0500

    Se arregla error en test

--- src/ImageExampleImage.js
+++ src/ImageExampleImage.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 
-const ImageExampleImage = () => (
+function ImageExampleImage () {
+  return(
+  <div>
   <Image src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
-  )
+  <a href='https://reactjs.org/tutorial/tutorial.html'>Learn react</a>
+  </div>)
+}
 
 export default ImageExampleImage
